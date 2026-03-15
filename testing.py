4f4a987ce439da21d8461f3c97c7b92560baacf8
Anuj Soni <sonianuj287@gmail.com>
2026-03-15 19:32:30 +0530

    removed testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA0ylWXX6JpUYgcD3_sSleRT8kEDQoksbU")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
